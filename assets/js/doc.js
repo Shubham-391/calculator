844e3e7744ee30d70f23eeb0f007279e96ffcf00
Shubham-391 <subhamy1234@gmail.com>
2024-02-20 15:27:59 +0530

    js

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -6,11 +6,7 @@ function num(x) {
   } else {
     output.innerHTML += x;
   }
-  document.getElementById("addition").classList.remove("pointer-none");
-  document.getElementById("minus").classList.remove("pointer-none");
-  document.getElementById("divide").classList.remove("pointer-none");
-  document.getElementById("multiply").classList.remove("pointer-none");
-  document.getElementById("dot").classList.remove("pointer-none");
+  document.querySelectorAll(".op").forEach((x) => x.classList.remove("pointer-none"));
 }
 function op(x) {
   if (output.innerHTML == 0) {
@@ -18,53 +14,29 @@ function op(x) {
   } else {
     output.innerHTML += x;
   }
-  document.getElementById("addition").classList.add("pointer-none");
-  document.getElementById("minus").classList.add("pointer-none");
-  document.getElementById("divide").classList.add("pointer-none");
-  document.getElementById("multiply").classList.add("pointer-none");
-  document.getElementById("dot").classList.add("pointer-none");
+  document.querySelectorAll(".op").forEach((x) => x.classList.add("pointer-none"));
 }
 function empty() {
-    output.innerHTML = 0;
-     document.getElementById("addition").classList.remove("pointer-none");
-     document.getElementById("minus").classList.remove("pointer-none");
-     document.getElementById("divide").classList.remove("pointer-none");
-     document.getElementById("multiply").classList.remove("pointer-none");
-     document.getElementById("dot").classList.remove("pointer-none");
+  output.innerHTML = 0;
+  document.querySelectorAll(".op").forEach((x) => x.classList.remove("pointer-none"));
 }
 function del() {
   if (output.innerHTML == 0) {
     output.innerHTML = 0;
   } else {
-    output.innerHTML = output.innerHTML.slice(0, -1);
-    }
-     document.getElementById("addition").classList.remove("pointer-none");
-     document.getElementById("minus").classList.remove("pointer-none");
-     document.getElementById("divide").classList.remove("pointer-none");
-     document.getElementById("multiply").classList.remove("pointer-none");
-     document.getElementById("dot").classList.remove("pointer-none");
+    output.innerHTML = output.innerHTML.slice(0, -1);    
+  }
+  document.querySelectorAll(".op").forEach((e) => e.classList.remove("pointer-none"));
 }
 function result() {
-    output.innerHTML = eval(output.innerHTML);
-     document.getElementById("addition").classList.remove("pointer-none");
-     document.getElementById("minus").classList.remove("pointer-none");
-     document.getElementById("divide").classList.remove("pointer-none");
-     document.getElementById("multiply").classList.remove("pointer-none");
-     document.getElementById("dot").classList.remove("pointer-none");
+  output.innerHTML = eval(output.innerHTML);
+  document.querySelectorAll(".op").forEach((e) => e.classList.remove("pointer-none"));
 }
 function power() {
-    output.innerHTML = Math.pow(parseFloat(output.innerHTML), 2);
-     document.getElementById("addition").classList.remove("pointer-none");
-     document.getElementById("minus").classList.remove("pointer-none");
-     document.getElementById("divide").classList.remove("pointer-none");
-     document.getElementById("multiply").classList.remove("pointer-none");
-     document.getElementById("dot").classList.remove("pointer-none");
+  output.innerHTML = Math.pow(parseFloat(output.innerHTML), 2);
+  document.querySelectorAll(".op").forEach((e) => e.classList.remove("pointer-none"));
 }
 function root() {
-    output.innerHTML = Math.sqrt(parseFloat(output.innerHTML));
-     document.getElementById("addition").classList.remove("pointer-none");
-     document.getElementById("minus").classList.remove("pointer-none");
-     document.getElementById("divide").classList.remove("pointer-none");
-     document.getElementById("multiply").classList.remove("pointer-none");
-     document.getElementById("dot").classList.remove("pointer-none");
+  output.innerHTML = Math.sqrt(parseFloat(output.innerHTML));
+ document.querySelectorAll(".op").forEach((x) => x.classList.remove("pointer-none"));
 }
